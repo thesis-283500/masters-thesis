8a5c5cf9edb9ea965c6dfbdd77714d6f522579c3
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-19 17:34:11 +0200

    chore: create ns

--- action-runner/setup.sh
+++ action-runner/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl apply -f cert-manager.yaml --server-side=true --force-conflicts
+kubectl create ns actions-runner-system
+kubectl label ns actions-runner-system control-plane=controller-manager
 kubectl apply -f controller-manager-secret.yaml
 kubectl apply -f actions-runner-controller.yaml --server-side=true --force-conflicts
-sleep 20
-kubectl apply -f runnerdeployment.yaml
