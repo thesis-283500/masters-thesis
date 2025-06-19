0cd55e3e2eec292a052cc16ea4c902ef06534bab
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-19 23:51:02 +0200

    fix: apply sleep

--- action-runner/setup.sh
+++ action-runner/setup.sh
@@ -2,5 +2,6 @@
 kubectl apply -f cert-manager.yaml --server-side=true --force-conflicts
 kubectl create ns actions-runner-system
 kubectl label ns actions-runner-system control-plane=controller-manager
+sleep 20
 kubectl apply -f controller-manager-secret.yaml
 kubectl apply -f actions-runner-controller.yaml --server-side=true --force-conflicts
