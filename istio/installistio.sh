8bca77b1facf5f6bcedccd00b4a0ed3f18c0ace3
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-21 13:21:17 +0200

    chore: use manifest

--- istio/installistio.sh
+++ istio/installistio.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 
-# point this to your latest build binary
-istioctl_latest=./istioctl
-
 # install certs in both clusters
 kubectl create namespace istio-system 
 
 kubectl apply -f gateway-crds.yaml
 kubectl label namespace istio-system topology.istio.io/network=thesis-283500
 
-istioctl install -f backend-io.yaml --skip-confirmation
+kubectl apply -f manifest.yaml
 
 sleep 30
 
