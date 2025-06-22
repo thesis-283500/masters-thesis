fa23e47eda11a7f77f4ade7f27323f7f26108fba
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-23 00:06:34 +0200

    chore: keep for demo

--- kiali/kiali.sh
+++ kiali/kiali.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-helm repo add kiali https://kiali.org/helm-charts
-helm repo update
-
-helm install \
-    --set cr.create=true \
-    --set cr.namespace=istio-system \
-    --set cr.spec.auth.strategy="anonymous" \
-    --namespace kiali-operator \
-    --create-namespace \
-    kiali-operator \
-    kiali/kiali-operator
+kubectl apply -f install.yaml
