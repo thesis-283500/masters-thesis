1e3a52a7d1dfa38cdcaa125b5cede4fed7a6f373
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-22 08:22:38 +0200

    chore: install kube-prometheus

--- action-runner/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl apply -f cert-manager.yaml --server-side=true --force-conflicts
-kubectl create ns actions-runner-system
-kubectl label ns actions-runner-system control-plane=controller-manager
-sleep 20
-kubectl apply -f controller-manager-secret.yaml
-kubectl apply -f actions-runner-controller.yaml --server-side=true --force-conflicts
