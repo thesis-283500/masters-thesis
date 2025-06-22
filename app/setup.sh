787841d01fe1b9b009572caf85bfca1cebc510bb
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-22 11:39:20 +0200

    chore: clean up implementation

--- app/setup.sh
+++ app/setup.sh
@@ -2,4 +2,6 @@
 
 kubectl create ns microservice
 kubectl label ns microservice istio-injection=enabled
+kubectl apply -f httproute-crd.yaml
+kubectl apply -f istio-manifests.yaml
 kubectl apply -n microservice -f kubernetes-manifests.yaml
