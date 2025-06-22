0d1ba4a2b9a26ca89f1958002fdad14ca2de9b0f
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-22 12:47:25 +0200

    chore: make app to work

--- view-endpoints.sh
+++ view-endpoints.sh
@@ -4,4 +4,5 @@ kubectl port-forward --address localhost -n kepler service/kepler-exporter 9102:
 kubectl port-forward --address localhost -n monitoring service/prometheus-k8s 9090:9090 &
 kubectl port-forward --address localhost -n monitoring service/grafana 3000:3000 &
 kubectl port-forward --address localhost -n istio-system service/kiali 20001:20001 &
-kubectl get service frontend-external | awk '{print $4}' &
+kubectl port-forward --address localhost -n microservice service/frontend 8080:80 &
+# kubectl get -n microservice service frontend-external | awk '{print $4}' &
