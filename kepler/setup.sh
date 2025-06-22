7da0c225baff34532e81ee9a0f5e801a750eff8e
WUST-Projects <283500@student.pwr.edu.pl>
2025-06-22 08:36:53 +0200

    chore: install kiali and kepler

--- kepler/setup.sh
+++ kepler/setup.sh
@@ -2,7 +2,3 @@
 
 kubectl apply -f deployment.yaml
 sleep 5
-
-kubectl port-forward --address localhost -n kepler service/kepler-exporter 9102:9102 &
-kubectl port-forward --address localhost -n monitoring service/prometheus-k8s 9090:9090 &
-kubectl port-forward --address localhost -n monitoring service/grafana 3000:3000 &
